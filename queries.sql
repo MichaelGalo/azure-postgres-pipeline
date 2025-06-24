c3dec07cb1e672cf41e7132cbaa19f91f69b19de
Michael Galo <michael@michaelthinks.co>
2025-06-24 14:16:08 -0500

    added .sql notes for future use

--- queries.sql
+++
@@ -1,7 +0,0 @@
--- Subset Table Creation Query
-
-
--- Stored Proc of Subset
-
-
--- Queries off of the Subset for Reporting
---
+++ subset_table.sql
@@ -0,0 +1,7 @@
+-- For each table affected, create a new .sql file with the following structure:
+
+-- Subset Table Creation Query
+
+-- Stored Proc of Subset
+
+
